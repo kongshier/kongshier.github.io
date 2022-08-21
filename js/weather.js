ac014c81b7e7847419144dc0aa4432a3722f7667
kongshier <2927527234@qq.com>
2022-08-21 14:51:05 +0800

    Site updated: 2022-08-21 14:51:04

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "FF9900",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "0px 0px 0px 0px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "true",
+      "vertical": "center",
+      "horizontal": "center",
+      "left": "125",
+      "top": "20",
+      "key": "d01fb2bd4a3244c28093e2f8300c6f6b"
+    }
+  }
